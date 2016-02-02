88294ca82f3f13202187ee43c10efcdd77c14d67
P.Kernevez <pkernevez@octo.com>
2016-02-02 14:33:43 +0100

    Day 3

--- dojox_python/configuration.py
+++ dojox_python/configuration.py
@@ -4,10 +4,10 @@
 from __future__ import print_function
 
 import os
-import sys
-import traceback
-import yaml
 import re
+import sys
+
+import yaml
 
 sys.path.insert(0, os.getcwd())
 
@@ -15,40 +15,37 @@ sys.stderr = sys.stdout
 
 
 class Configuration:
-
-
-
     def __init__(self, filename):
         self.filename = filename
         with open(filename, 'r') as stream:
             self.environments = yaml.load(stream)
 
-        for env,servers in self.environments.items():
-            for server,urls in servers.items():
+        for env, servers in self.environments.items():
+            for server, urls in servers.items():
                 for url in urls:
                     protocol = Configuration.protocol(url)
-                    if  protocol not in ['jar', 'file']:
+                    if protocol not in ['jar', 'file']:
                         raise InvalidConfiguration(protocol + ' not supported')
 
     def protocol(url):
-         return re.search('^(.*):', url).group(1)
+        return re.search('^(.*):', url).group(1)
 
-class InvalidConfiguration (Exception):
+
+class InvalidConfiguration(Exception):
     def __init__(self, errorMsg):
         pass
 
+
 class Manifest:
 
-    def __init__(self, filename):
-        self.filename = filename
-
-    def parse(self):
-        content = {}
-        with open(self.filename, 'r') as stream:
-            for line in stream:
+    def __init__(self, contentString):
+        self.content = {}
+        if contentString != '':
+            lines = contentString.split("\n")
+            for line in lines:
                 vals = line.split(": ")
-                content[vals[0]] = vals[1].strip('\n\r')
-        return content
+                self.content[vals[0]] = vals[1].strip('\n\r')
+                #TODO fro compèrehnsion
 
 
 class Connector:
@@ -56,27 +53,27 @@ class Connector:
         self.protocol = protocol
 
     def connect(self, url):
-        protocol = Configuration.protocol(url)
+        return ""
 
 
 class ConnectorManager:
     def __init__(self):
-        self.connectors = []
+        self.connectors = {}
 
     def register(self, connector):
-        self.connectors.append( connector)
+        self.connectors[connector.protocol] = connector
+        return self
 
+    def connect(self, url):
+        protocol = Configuration.protocol(url)
+        manifestContent = self.connectors[protocol].connect(url)
 
+        return manifestContent
 
-
-
-
-
-#if __name__ == '__main__':
-    #try:
-     #   ng = Configuration("aaa")
-     #   ng.run()
-    # except Exception as e:
-    #     sys.stderr.write("Error into reducer : " + traceback.format_exc() + "\n")
-    #     exit(-1)
-
+        # if __name__ == '__main__':
+        # try:
+        #   ng = Configuration("aaa")
+        #   ng.run()
+        # except Exception as e:
+        #     sys.stderr.write("Error into reducer : " + traceback.format_exc() + "\n")
+        #     exit(-1)
--- dojox_python/test/configurationTest.py
+++ dojox_python/test/configurationTest.py
@@ -7,6 +7,8 @@ import os
 import logging
 from  os.path import dirname
 from datetime import date
+from unittest.mock import MagicMock
+
 
 from configuration import *
 
@@ -46,24 +48,49 @@ class TestConfiguration(unittest.TestCase):
         except FileNotFoundError as e:
             self.assertTrue("No such file" in e.__str__())
 
-    def test_parseManifest(self):
-        manifest = Manifest("MANIFEST.MF")
-        manifestContent = manifest.parse()
-        self.assertIsNotNone(manifestContent)
-        self.assertEqual('origin/master', manifestContent['Job-Name'])
+
+    def test_parseEmptyManifest(self):
+        manifest = Manifest("")
+        self.assertEqual({}, manifest.content )
+
+    def test_parseManifestWithOneCorrectLine(self):
+        manifest = Manifest("Job-Name: origin/master")
+        self.assertIsNotNone(manifest.content)
+        self.assertEqual('origin/master', manifest.content['Job-Name'])
+
+    def test_parseManifestWithTwoCorrectLines(self):
+        manifest = Manifest("Job-Name: origin/master\nBuildDate: 2016-01-08 17:26")
+        self.assertIsNotNone(manifest.content)
+        self.assertEqual(2, manifest.content.__len__())
+        self.assertEqual('origin/master', manifest.content['Job-Name'])
+        self.assertEqual('2016-01-08 17:26', manifest.content['BuildDate'])
 
     def test_file_connector(self):
         connector = Connector('file')
         connector.connect('file:/blah/blah')
-
-
 
     def test_registering_of_connector(self):
         mgr = ConnectorManager()
         mgr.register( MockConnector('jar', 'Job-Name: origin/master') )
         self.assertEqual(1, len( mgr.connectors))
 
+    def test_managerConnectToAppropriateConnector(self):
+        mgr = ConnectorManager()
+        jarConnector = MockConnector('jar', 'Job-Name: origin/master')
+        jarConnector.connect = MagicMock(return_value="")
+        fileConnector = MockConnector('file', 'Job-Name: origin/master')
+        mgr.register( jarConnector ).register( fileConnector )
+        mgr.connect("jar:/data/appli2/programme.jar#META-INF/MANIFEST")
+        jarConnector.connect.assert_called_with("jar:/data/appli2/programme.jar#META-INF/MANIFEST")
 
+    def test_managerConnectorReturnsValidManifest(self):
+        mgr = ConnectorManager()
+        jarConnector = MockConnector('jar', 'Job-Name: origin/master')
+        jarConnector.connect = MagicMock(return_value="BuildDate: 2016-01-08 17:26\nMain-Class: com.dojo.Main")
+        mgr.register( jarConnector )
+        manifest = mgr.connect("jar:/data/appli2/programme.jar#META-INF/MANIFEST")
+        self.assertIsNotNone(manifest)
+        #self.assertEqual({"BuildDate":"2016-01-08 17:26","Main-Class":"com.dojo.Main"}, manifest)
 
 class MockConnector (Connector):
     def __init__(self, protocol, data):
@@ -75,3 +102,4 @@ class MockConnector (Connector):
 
 if __name__ == '__main__':
     unittest.main()
+# jar : unzip -p jdiff.jar META-INF/MANIFEST.MF
